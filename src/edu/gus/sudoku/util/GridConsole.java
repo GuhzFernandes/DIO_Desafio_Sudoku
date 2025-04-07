a3f8d31e04ed9d2ea44065cc927cc9dd6d17de4f
Gustavo Fernandes <gustavofernandes330@gmail.com>
2025-04-07 18:58:56 -0300

    Adionada DB de jogos em resources - Teste de Grid para console

---
+++ src/edu/gus/sudoku/util/GridConsole.java
@@ -0,0 +1,21 @@
+package edu.gus.sudoku.util;
+
+public class GridConsole {
+    public static String PRINT =
+                    "=========   SUDOKU  =========\n\n"+
+                    "    A B C | D E F | G H I\n"+
+                    "  --------|-------|-------\n" +
+                    "1 | %s %s %s | %s %s %s | %s %s %s |\n"+
+                    "2 | %s %s %s | %s %s %s | %s %s %s |\n"+
+                    "3 | %s %s %s | %s %s %s | %s %s %s |\n"+
+                    "  --------|-------|-------\n" +
+                    "4 | %s %s %s | %s %s %s | %s %s %s |\n"+
+                    "5 | %s %s %s | %s %s %s | %s %s %s |\n"+
+                    "6 | %s %s %s | %s %s %s | %s %s %s |\n"+
+                    "  --------|-------|-------\n" +
+                    "7 | %s %s %s | %s %s %s | %s %s %s |\n"+
+                    "8 | %s %s %s | %s %s %s | %s %s %s |\n"+
+                    "9 | %s %s %s | %s %s %s | %s %s %s |\n"+
+                    "  --------|-------|-------\n";
+
+}
